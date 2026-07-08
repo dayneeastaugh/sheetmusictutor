39280e51df66042915c0e88dff39d573ca44ff3c
Dayne Eastaugh <dayne.eastaugh@gmail.com>
2026-07-08 10:55:08 +0400

    Initial Commit

---
+++ Woodshed/WoodshedApp.swift
@@ -0,0 +1,17 @@
+//
+//  WoodshedApp.swift
+//  Woodshed
+//
+//  Created by Dayne Eastaugh on 8/7/2026.
+//
+
+import SwiftUI
+
+@main
+struct WoodshedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
